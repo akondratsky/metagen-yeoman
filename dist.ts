5efabfb26608be18724f2b72c49918e83198fc6e
Alexandr Kondratskii <akondratsky@yandex.ru>
2024-03-17 17:21:46 +0100

    Publish both ESM and CommonJS modules (#4)
    * fix: add both ESM and CJS modules into the library
    
    * chore: revert version change in package.json
    
    * ci: rename oven-sh/setup-bun param
    
    * ci: update param name in oven-sh/setup-node
    
    ---------
    
    Co-authored-by: Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>

--- dist.ts
+++ dist.ts
@@ -1,8 +1,25 @@
 import fs from 'node:fs';
 
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+fs.writeFileSync('./dist/cjs/package.json', '{ "type": "commonjs" }');
+fs.writeFileSync('./dist/mjs/package.json', '{ "type": "module" }');
+fs.copyFileSync('.npmignore', './dist/.npmignore');
+fs.copyFileSync('LICENSE', './dist/LICENSE');
+
+const packageJson = {
+  ...JSON.parse(fs.readFileSync('package.json', 'utf-8')),
+  main: 'cjs/index.js',
+  module: 'mjs/index.js',
+  types: 'cjs/index.d.ts',
+  exports: {
+    '.': {
+      import: './mjs/index.js',
+      require: './cjs/index.js',
+    },
+  },
+};
+
 delete packageJson.devDependencies;
 delete packageJson.scripts;
-fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2), 'utf-8');
 
-fs.copyFileSync('LICENSE', './dist/LICENSE');
+
+fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2), 'utf-8');
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { Generator } from './Generator';
 
-export default Generator;
+export {
+  Generator,
+};
